2303d40a06d587c8a9bbfc9e12b31bd412ea1fe1
Tiago Deliberali Santos <tiago.deliberali@gmail.com>
2026-02-09 14:40:04 -0300

    implement repl

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut user_input = String::new();
+        let mut user_input = String::new();
 
-    io::stdin().read_line(&mut user_input).expect("Invalid input");
-    let user_input = user_input.trim();
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Invalid input");
+        let user_input = user_input.trim();
 
-    print!("{user_input}: command not found");
+        println!("{user_input}: command not found");
+    }
 }
