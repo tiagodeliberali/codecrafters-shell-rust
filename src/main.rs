305f97fe7e86975ecebd279236b0f524c15c482d
Tiago Deliberali Santos <tiago.deliberali@gmail.com>
2026-02-09 14:36:17 -0300

    print command not found

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,11 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut user_input = String::new();
+
+    io::stdin().read_line(&mut user_input).expect("Invalid input");
+    let user_input = user_input.trim();
+
+    print!("{user_input}: command not found");
 }
