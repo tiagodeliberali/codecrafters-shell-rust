2cf55ec846624caa276b8352b8dd48a4f8303ebf
Tiago Deliberali Santos <tiago.deliberali@gmail.com>
2026-02-15 20:00:45 -0300

    implement tab

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ mod commands;
 mod parser;
 mod shell;
 
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind};
+use crossterm::terminal::ClearType;
 // use owo_colors::OwoColorize;
+use crossterm::{cursor, event, execute, terminal};
 use std::collections::HashMap;
+use std::env;
 use std::io::{self, Write};
-use std::path::{PathBuf};
-use std::{env};
+use std::path::PathBuf;
 
 use crate::shell::{CommandInput, CommandOutput, processor};
 
@@ -28,16 +31,10 @@ fn main() {
     commands.insert("dir", commands::ls);
     commands.insert("type", commands::type_fn);
 
+    let know_commands = commands.keys().map(|i| i.to_string()).collect();
+
     loop {
-        print!("$ ");
-        io::stdout().flush().unwrap();
-
-        let mut user_input = String::new();
-
-        io::stdin()
-            .read_line(&mut user_input)
-            .expect("Invalid input");
-
+        let user_input = retrieve_user_input(&know_commands);
         let command = user_input.trim();
         let words = parser::parse_input(command);
 
@@ -74,6 +71,89 @@ fn main() {
     }
 }
 
+fn retrieve_user_input(know_commands: &Vec<String>) -> String {
+    let prompt = "$ ";
+    print!("{prompt}");
+    io::stdout().flush().unwrap();
 
+    terminal::enable_raw_mode().unwrap();
+    let mut user_input = String::new(); // what the user has typed so far
+    let mut cursor_pos: usize = 0; // cursor position in the string
 
+    loop {
+        let event = event::read().unwrap();
 
+        if let Event::Key(KeyEvent {
+            code,
+            kind: KeyEventKind::Press,
+            ..
+        }) = event
+        {
+            match code {
+                KeyCode::Char(c) => {
+                    user_input.insert(cursor_pos, c);
+                    cursor_pos += 1;
+                    redraw_line(prompt, &user_input, cursor_pos);
+                }
+                KeyCode::Left => {
+                    if cursor_pos > 0 {
+                        cursor_pos -= 1;
+                    }
+                    redraw_line(prompt, &user_input, cursor_pos);
+                }
+                KeyCode::Right => {
+                    if cursor_pos < user_input.len() {
+                        cursor_pos += 1;
+                    }
+                    redraw_line(prompt, &user_input, cursor_pos);
+                }
+                KeyCode::Backspace => {
+                    if cursor_pos > 0 {
+                        cursor_pos -= 1;
+                        user_input.remove(cursor_pos);
+                        redraw_line(prompt, &user_input, cursor_pos);
+                    }
+                }
+                KeyCode::Tab => {
+                    let found_command: Vec<&String> = know_commands.iter().filter(|i| i.starts_with(&user_input)).collect();
+                    let found_command = found_command.first();
+
+                    if let Some(command_name) = found_command {
+                        user_input = command_name.to_string();
+                        cursor_pos = user_input.len();
+                        redraw_line(prompt, &user_input, cursor_pos);
+                    }
+                }
+                KeyCode::Enter => {
+                    print!("\r\n");
+                    break;
+                }
+                _ => {
+                    continue;
+                }
+            }
+        }
+    }
+
+    terminal::disable_raw_mode().unwrap();
+
+    user_input
+}
+
+fn redraw_line(prompt: &str, input: &str, cursor_pos: usize) {
+    // print content
+    let mut stdout = io::stdout();
+
+    print!("\r");
+    execute!(stdout, terminal::Clear(ClearType::CurrentLine)).unwrap();
+    print!("{prompt}{input}");
+
+    // position cursos
+    print!("\r");
+    let target_col = prompt.len() + cursor_pos;
+    if target_col > 0 {
+        execute!(stdout, cursor::MoveRight(target_col as u16)).unwrap();
+    }
+
+    stdout.flush().unwrap();
+}
