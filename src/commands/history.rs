b0bc6c452cbeb7b5b3b72387a3ea8d286d0537ae
Tiago Deliberali Santos <tiago.deliberali@gmail.com>
2026-02-21 10:44:16 -0300

    define limit

--- src/commands/history.rs
+++ src/commands/history.rs
@@ -1,9 +1,28 @@
+use std::num::ParseIntError;
+
 use crate::shell::{CommandInput, CommandOutput};
 
 pub fn history(input: CommandInput) -> CommandOutput {
+    let size: Option<Result<usize, ParseIntError>> = input.command_arguments.first().map(|s| s.as_str().parse());
+
+    let (initial_value, enumeration) = match size {
+        None => (1, input.command_history.iter().enumerate()),
+        Some(value) => {
+            let value = match value {
+                Err(error) => {
+                    return CommandOutput::failure(format!("Failed to parse history argument: {}", error.to_string()))
+                }
+                Ok(v) => v
+            };
+
+            let start_position: usize = input.command_history.len().saturating_sub(value);
+            (start_position + 1, input.command_history[start_position..].iter().enumerate())
+        }
+    };
+
     let mut output = String::new();
-    for (position, command) in input.command_history.iter().enumerate() {
-        output += format!("{} {}\n", (position + 1), command).as_str();
+    for (position, command) in enumeration {
+        output += format!("{} {}\n", (position + initial_value), command).as_str();
     }
     CommandOutput::success(output)
 }
