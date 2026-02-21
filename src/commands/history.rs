dd2fd5aa715fdce25921d666d027953a063bdc38
Tiago Deliberali Santos <tiago.deliberali@gmail.com>
2026-02-21 11:48:54 -0300

    implement history write

--- src/commands/history.rs
+++ src/commands/history.rs
@@ -3,41 +3,65 @@ use std::{fs, num::ParseIntError};
 use crate::shell::{CommandInput, CommandOutput};
 
 pub fn history(input: CommandInput) -> CommandOutput {
-    if let Some(arg) = input.command_arguments.first() && arg == "-r" {
-        let result = fs::read_to_string(input.command_arguments.get(1).unwrap_or(&String::new()));
+    if let Some(arg) = input.command_arguments.first() {
+        if arg == "-r" {
+            let result =
+                fs::read_to_string(input.command_arguments.get(1).unwrap_or(&String::new()));
 
-        match result {
-            Err(error) => {
-                return CommandOutput::failure(format!("Failed to read history file: {}", error.to_string()));
-            }
-            Ok(result) => {
-                let mut paths: Vec<String> = Vec::new();
-
-                for line in result.lines() {
-                    if !line.is_empty() {
-                        paths.push(line.to_string());
-                    }
+            match result {
+                Err(error) => {
+                    return CommandOutput::failure(format!(
+                        "Failed to read history file: {}",
+                        error.to_string()
+                    ));
                 }
+                Ok(result) => {
+                    let mut paths: Vec<String> = Vec::new();
 
-                return CommandOutput::history_update(paths);
+                    for line in result.lines() {
+                        if !line.is_empty() {
+                            paths.push(line.to_string());
+                        }
+                    }
+
+                    return CommandOutput::history_update(paths);
+                }
+            }
+        } else if arg == "-w" {
+            match fs::write(
+                input.command_arguments.get(1).unwrap_or(&String::new()), 
+                format!("{}\n", input.command_history.join("\n"))) {
+                Err(error) => {
+                    return CommandOutput::failure(format!("Failed to write history: {}", error.to_string()));
+                }
+                Ok(_) => {
+                    return CommandOutput::empty();
+                }
             }
         }
     }
 
-    let size: Option<Result<usize, ParseIntError>> = input.command_arguments.first().map(|s| s.as_str().parse());
+    let size: Option<Result<usize, ParseIntError>> =
+        input.command_arguments.first().map(|s| s.as_str().parse());
 
     let (initial_value, enumeration) = match size {
         None => (1, input.command_history.iter().enumerate()),
         Some(value) => {
             let value = match value {
                 Err(error) => {
-                    return CommandOutput::failure(format!("Failed to parse history argument: {}", error.to_string()))
+                    return CommandOutput::failure(format!(
+                        "Failed to parse history argument: {}",
+                        error.to_string()
+                    ));
                 }
-                Ok(v) => v
+                Ok(v) => v,
             };
 
             let start_position: usize = input.command_history.len().saturating_sub(value);
-            (start_position + 1, input.command_history[start_position..].iter().enumerate())
+            (
+                start_position + 1,
+                input.command_history[start_position..].iter().enumerate(),
+            )
         }
     };
 
